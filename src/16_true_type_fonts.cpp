da3a0accdb0b39e3b02497db25ad93157cdb6397
gongluck <1039994845@qq.com>
2019-05-28 17:51:32 +0800

    添加true_type_fonts.cpp

--- src/16_true_type_fonts.cpp
+++ src/16_true_type_fonts.cpp
@@ -1,9 +1,10 @@
 /*This source code copyrighted by Lazy Foo' Productions (2004-2019)
 and may not be redistributed without written permission.*/
 
-//Using SDL, SDL_image, standard IO, math, and strings
+//Using SDL, SDL_image, SDL_ttf, standard IO, math, and strings
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <stdio.h>
 #include <string>
 #include <cmath>
@@ -13,17 +14,20 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 //Texture wrapper class
-class LTexture_rotation_and_flipping
+class LTexture_true_type_fonts
 {
 	public:
 		//Initializes variables
-		LTexture_rotation_and_flipping();
+		LTexture_true_type_fonts();
 
 		//Deallocates memory
-		~LTexture_rotation_and_flipping();
+		~LTexture_true_type_fonts();
 
 		//Loads image at specified path
 		bool loadFromFile( std::string path );
+		
+		//Creates image from font string
+		bool loadFromRenderedText( std::string textureText, SDL_Color textColor );
 
 		//Deallocates texture
 		void free();
@@ -54,25 +58,28 @@ class LTexture_rotation_and_flipping
 };
 
 //Starts up SDL and creates window
-bool init_rotation_and_flipping();
+bool init_true_type_fonts();
 
 //Loads media
-bool loadMedia_rotation_and_flipping();
+bool loadMedia_true_type_fonts();
 
 //Frees media and shuts down SDL
-void close_rotation_and_flipping();
+void close_true_type_fonts();
 
 //The window we'll be rendering to
-SDL_Window* gWindow_rotation_and_flipping = NULL;
+SDL_Window* gWindow_true_type_fonts = NULL;
 
 //The window renderer
-SDL_Renderer* gRenderer_rotation_and_flipping = NULL;
+SDL_Renderer* gRenderer_true_type_fonts = NULL;
 
-//Scene texture
-LTexture_rotation_and_flipping gArrowTexture_rotation_and_flipping;
+//Globally used font
+TTF_Font *gFont_true_type_fonts = NULL;
+
+//Rendered texture
+LTexture_true_type_fonts gTextTexture_true_type_fonts;
 
 
-LTexture_rotation_and_flipping::LTexture_rotation_and_flipping()
+LTexture_true_type_fonts::LTexture_true_type_fonts()
 {
 	//Initialize
 	mTexture = NULL;
@@ -80,13 +87,13 @@ LTexture_rotation_and_flipping::LTexture_rotation_and_flipping()
 	mHeight = 0;
 }
 
-LTexture_rotation_and_flipping::~LTexture_rotation_and_flipping()
+LTexture_true_type_fonts::~LTexture_true_type_fonts()
 {
 	//Deallocate
 	free();
 }
 
-bool LTexture_rotation_and_flipping::loadFromFile( std::string path )
+bool LTexture_true_type_fonts::loadFromFile( std::string path )
 {
 	//Get rid of preexisting texture
 	free();
@@ -106,7 +113,7 @@ bool LTexture_rotation_and_flipping::loadFromFile( std::string path )
 		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
 
 		//Create texture from surface pixels
-        newTexture = SDL_CreateTextureFromSurface( gRenderer_rotation_and_flipping, loadedSurface );
+        newTexture = SDL_CreateTextureFromSurface( gRenderer_true_type_fonts, loadedSurface );
 		if( newTexture == NULL )
 		{
 			printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
@@ -127,7 +134,41 @@ bool LTexture_rotation_and_flipping::loadFromFile( std::string path )
 	return mTexture != NULL;
 }
 
-void LTexture_rotation_and_flipping::free()
+bool LTexture_true_type_fonts::loadFromRenderedText( std::string textureText, SDL_Color textColor )
+{
+	//Get rid of preexisting texture
+	free();
+
+	//Render text surface
+	SDL_Surface* textSurface = TTF_RenderText_Solid( gFont_true_type_fonts, textureText.c_str(), textColor );
+	if( textSurface == NULL )
+	{
+		printf( "Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError() );
+	}
+	else
+	{
+		//Create texture from surface pixels
+        mTexture = SDL_CreateTextureFromSurface( gRenderer_true_type_fonts, textSurface );
+		if( mTexture == NULL )
+		{
+			printf( "Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError() );
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = textSurface->w;
+			mHeight = textSurface->h;
+		}
+
+		//Get rid of old surface
+		SDL_FreeSurface( textSurface );
+	}
+	
+	//Return success
+	return mTexture != NULL;
+}
+
+void LTexture_true_type_fonts::free()
 {
 	//Free texture if it exists
 	if( mTexture != NULL )
@@ -139,25 +180,25 @@ void LTexture_rotation_and_flipping::free()
 	}
 }
 
-void LTexture_rotation_and_flipping::setColor( Uint8 red, Uint8 green, Uint8 blue )
+void LTexture_true_type_fonts::setColor( Uint8 red, Uint8 green, Uint8 blue )
 {
 	//Modulate texture rgb
 	SDL_SetTextureColorMod( mTexture, red, green, blue );
 }
 
-void LTexture_rotation_and_flipping::setBlendMode( SDL_BlendMode blending )
+void LTexture_true_type_fonts::setBlendMode( SDL_BlendMode blending )
 {
 	//Set blending function
 	SDL_SetTextureBlendMode( mTexture, blending );
 }
 		
-void LTexture_rotation_and_flipping::setAlpha( Uint8 alpha )
+void LTexture_true_type_fonts::setAlpha( Uint8 alpha )
 {
 	//Modulate texture alpha
 	SDL_SetTextureAlphaMod( mTexture, alpha );
 }
 
-void LTexture_rotation_and_flipping::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
+void LTexture_true_type_fonts::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
 {
 	//Set rendering space and render to screen
 	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
@@ -170,20 +211,20 @@ void LTexture_rotation_and_flipping::render( int x, int y, SDL_Rect* clip, doubl
 	}
 
 	//Render to screen
-	SDL_RenderCopyEx( gRenderer_rotation_and_flipping, mTexture, clip, &renderQuad, angle, center, flip );
+	SDL_RenderCopyEx( gRenderer_true_type_fonts, mTexture, clip, &renderQuad, angle, center, flip );
 }
 
-int LTexture_rotation_and_flipping::getWidth()
+int LTexture_true_type_fonts::getWidth()
 {
 	return mWidth;
 }
 
-int LTexture_rotation_and_flipping::getHeight()
+int LTexture_true_type_fonts::getHeight()
 {
 	return mHeight;
 }
 
-bool init_rotation_and_flipping()
+bool init_true_type_fonts()
 {
 	//Initialization flag
 	bool success = true;
@@ -203,8 +244,8 @@ bool init_rotation_and_flipping()
 		}
 
 		//Create window
-		gWindow_rotation_and_flipping = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow_rotation_and_flipping == NULL )
+		gWindow_true_type_fonts = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow_true_type_fonts == NULL )
 		{
 			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
 			success = false;
@@ -212,8 +253,8 @@ bool init_rotation_and_flipping()
 		else
 		{
 			//Create vsynced renderer for window
-			gRenderer_rotation_and_flipping = SDL_CreateRenderer( gWindow_rotation_and_flipping, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-			if( gRenderer_rotation_and_flipping == NULL )
+			gRenderer_true_type_fonts = SDL_CreateRenderer( gWindow_true_type_fonts, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+			if( gRenderer_true_type_fonts == NULL )
 			{
 				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
 				success = false;
@@ -221,13 +262,20 @@ bool init_rotation_and_flipping()
 			else
 			{
 				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer_rotation_and_flipping, 0xFF, 0xFF, 0xFF, 0xFF );
+				SDL_SetRenderDrawColor( gRenderer_true_type_fonts, 0xFF, 0xFF, 0xFF, 0xFF );
 
 				//Initialize PNG loading
 				int imgFlags = IMG_INIT_PNG;
 				if( !( IMG_Init( imgFlags ) & imgFlags ) )
 				{
 					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					success = false;
+				}
+
+				 //Initialize SDL_ttf
+				if( TTF_Init() == -1 )
+				{
+					printf( "SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError() );
 					success = false;
 				}
 			}
@@ -237,48 +285,64 @@ bool init_rotation_and_flipping()
 	return success;
 }
 
-bool loadMedia_rotation_and_flipping()
+bool loadMedia_true_type_fonts()
 {
 	//Loading success flag
 	bool success = true;
 
-	//Load arrow
-	if( !gArrowTexture_rotation_and_flipping.loadFromFile( "arrow.png" ) )
+	//Open the font
+	gFont_true_type_fonts = TTF_OpenFont( "lazy.ttf", 28 );
+	if( gFont_true_type_fonts == NULL )
 	{
-		printf( "Failed to load arrow texture!\n" );
+		printf( "Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError() );
 		success = false;
+	}
+	else
+	{
+		//Render text
+		SDL_Color textColor = { 0, 0, 0 };
+		if( !gTextTexture_true_type_fonts.loadFromRenderedText( "The quick brown fox jumps over the lazy dog", textColor ) )
+		{
+			printf( "Failed to render text texture!\n" );
+			success = false;
+		}
 	}
 
 	return success;
 }
 
-void close_rotation_and_flipping()
+void close_true_type_fonts()
 {
 	//Free loaded images
-	gArrowTexture_rotation_and_flipping.free();
+	gTextTexture_true_type_fonts.free();
+
+	//Free global font
+	TTF_CloseFont( gFont_true_type_fonts);
+	gFont_true_type_fonts = NULL;
 
 	//Destroy window	
-	SDL_DestroyRenderer( gRenderer_rotation_and_flipping);
-	SDL_DestroyWindow( gWindow_rotation_and_flipping);
-	gWindow_rotation_and_flipping = NULL;
-	gRenderer_rotation_and_flipping = NULL;
-	
+	SDL_DestroyRenderer( gRenderer_true_type_fonts);
+	SDL_DestroyWindow( gWindow_true_type_fonts);
+	gWindow_true_type_fonts = NULL;
+	gRenderer_true_type_fonts = NULL;
+
 	//Quit SDL subsystems
+	TTF_Quit();
 	IMG_Quit();
 	SDL_Quit();
 }
 
-int main_rotation_and_flipping( int argc, char* args[] )
+int main( int argc, char* args[] )
 {
 	//Start up SDL and create window
-	if( !init_rotation_and_flipping() )
+	if( !init_true_type_fonts() )
 	{
 		printf( "Failed to initialize!\n" );
 	}
 	else
 	{
 		//Load media
-		if( !loadMedia_rotation_and_flipping() )
+		if( !loadMedia_true_type_fonts() )
 		{
 			printf( "Failed to load media!\n" );
 		}
@@ -289,12 +353,6 @@ int main_rotation_and_flipping( int argc, char* args[] )
 
 			//Event handler
 			SDL_Event e;
-
-			//Angle of rotation
-			double degrees = 0;
-
-			//Flip type
-			SDL_RendererFlip flipType = SDL_FLIP_NONE;
 
 			//While application is running
 			while( !quit )
@@ -307,50 +365,23 @@ int main_rotation_and_flipping( int argc, char* args[] )
 					{
 						quit = true;
 					}
-					else if( e.type == SDL_KEYDOWN )
-					{
-						switch( e.key.keysym.sym )
-						{
-							case SDLK_a:
-							degrees -= 60;
-							break;
-							
-							case SDLK_d:
-							degrees += 60;
-							break;
-
-							case SDLK_q:
-							flipType = SDL_FLIP_HORIZONTAL;
-							break;
-
-							case SDLK_w:
-							flipType = SDL_FLIP_NONE;
-							break;
-
-							case SDLK_e:
-							flipType = SDL_FLIP_VERTICAL;
-							break;
-						}
-					}
 				}
 
 				//Clear screen
-				SDL_SetRenderDrawColor( gRenderer_rotation_and_flipping, 0xFF, 0xFF, 0xFF, 0xFF );
-				SDL_RenderClear( gRenderer_rotation_and_flipping);
+				SDL_SetRenderDrawColor( gRenderer_true_type_fonts, 0xFF, 0xFF, 0xFF, 0xFF );
+				SDL_RenderClear( gRenderer_true_type_fonts);
 
-				//Render arrow
-				gArrowTexture_rotation_and_flipping.render( ( SCREEN_WIDTH - gArrowTexture_rotation_and_flipping.getWidth() ) / 2, ( SCREEN_HEIGHT - gArrowTexture_rotation_and_flipping.getHeight() ) / 2, NULL, degrees, NULL, flipType );
+				//Render current frame
+				gTextTexture_true_type_fonts.render( ( SCREEN_WIDTH - gTextTexture_true_type_fonts.getWidth() ) / 2, ( SCREEN_HEIGHT - gTextTexture_true_type_fonts.getHeight() ) / 2 );
 
 				//Update screen
-				SDL_RenderPresent( gRenderer_rotation_and_flipping);
+				SDL_RenderPresent( gRenderer_true_type_fonts);
 			}
 		}
 	}
 
 	//Free resources and close SDL
-	close_rotation_and_flipping
-    
-    ();
+	close_true_type_fonts();
 
 	return 0;
 }
