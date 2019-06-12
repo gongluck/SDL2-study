b2604abe81b475f02e82a707e08cb3fb5e6a0471
gongluck <1039994845@qq.com>
2019-06-12 14:24:32 +0800

    添加atomic_operations.cpp

--- src/48_atomic_operations.cpp
+++ src/48_atomic_operations.cpp
@@ -13,14 +13,14 @@ const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 //Texture wrapper class
-class LTexture_semaphores
+class LTexture_atomic_operations
 {
 	public:
 		//Initializes variables
-		LTexture_semaphores();
+		LTexture_atomic_operations();
 
 		//Deallocates memory
-		~LTexture_semaphores();
+		~LTexture_atomic_operations();
 
 		//Loads image at specified path
 		bool loadFromFile( std::string path );
@@ -75,33 +75,33 @@ class LTexture_semaphores
 };
 
 //Starts up SDL and creates window
-bool init_semaphores();
+bool init_atomic_operations();
 
 //Loads media
-bool loadMedia_semaphores();
+bool loadMedia_atomic_operations();
 
 //Frees media and shuts down SDL
-void close_semaphores();
+void close_atomic_operations();
 
-//Our worker thread function
-int worker_semaphores( void* data );
+//Our worker function
+int worker_atomic_operations( void* data );
 
 //The window we'll be rendering to
-SDL_Window* gWindow_semaphores = NULL;
+SDL_Window* gWindow_atomic_operations = NULL;
 
 //The window renderer
-SDL_Renderer* gRenderer_semaphores = NULL;
+SDL_Renderer* gRenderer_atomic_operations = NULL;
 
 //Scene textures
-LTexture_semaphores gSplashTexture_semaphores;
+LTexture_atomic_operations gSplashTexture_atomic_operations;
 
-//Data access semaphore
-SDL_sem* gDataLock_semaphores = NULL;
+//Data access spin lock
+SDL_SpinLock gDataLock_atomic_operations = -1;
 
 //The "data buffer"
-int gData_semaphores = -1;
+int gData_atomic_operations = -1;
 
-LTexture_semaphores::LTexture_semaphores()
+LTexture_atomic_operations::LTexture_atomic_operations()
 {
 	//Initialize
 	mTexture = NULL;
@@ -111,13 +111,13 @@ LTexture_semaphores::LTexture_semaphores()
 	mPitch = 0;
 }
 
-LTexture_semaphores::~LTexture_semaphores()
+LTexture_atomic_operations::~LTexture_atomic_operations()
 {
 	//Deallocate
 	free();
 }
 
-bool LTexture_semaphores::loadFromFile( std::string path )
+bool LTexture_atomic_operations::loadFromFile( std::string path )
 {
 	//Get rid of preexisting texture
 	free();
@@ -142,7 +142,7 @@ bool LTexture_semaphores::loadFromFile( std::string path )
 		else
 		{
 			//Create blank streamable texture
-			newTexture = SDL_CreateTexture( gRenderer_semaphores, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, formattedSurface->w, formattedSurface->h );
+			newTexture = SDL_CreateTexture( gRenderer_atomic_operations, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, formattedSurface->w, formattedSurface->h );
 			if( newTexture == NULL )
 			{
 				printf( "Unable to create blank texture! SDL Error: %s\n", SDL_GetError() );
@@ -234,10 +234,10 @@ bool LTexture::loadFromRenderedText( std::string textureText, SDL_Color textColo
 }
 #endif
 		
-bool LTexture_semaphores::createBlank( int width, int height, SDL_TextureAccess access )
+bool LTexture_atomic_operations::createBlank( int width, int height, SDL_TextureAccess access )
 {
 	//Create uninitialized texture
-	mTexture = SDL_CreateTexture( gRenderer_semaphores, SDL_PIXELFORMAT_RGBA8888, access, width, height );
+	mTexture = SDL_CreateTexture( gRenderer_atomic_operations, SDL_PIXELFORMAT_RGBA8888, access, width, height );
 	if( mTexture == NULL )
 	{
 		printf( "Unable to create blank texture! SDL Error: %s\n", SDL_GetError() );
@@ -251,7 +251,7 @@ bool LTexture_semaphores::createBlank( int width, int height, SDL_TextureAccess 
 	return mTexture != NULL;
 }
 
-void LTexture_semaphores::free()
+void LTexture_atomic_operations::free()
 {
 	//Free texture if it exists
 	if( mTexture != NULL )
@@ -265,25 +265,25 @@ void LTexture_semaphores::free()
 	}
 }
 
-void LTexture_semaphores::setColor( Uint8 red, Uint8 green, Uint8 blue )
+void LTexture_atomic_operations::setColor( Uint8 red, Uint8 green, Uint8 blue )
 {
 	//Modulate texture rgb
 	SDL_SetTextureColorMod( mTexture, red, green, blue );
 }
 
-void LTexture_semaphores::setBlendMode( SDL_BlendMode blending )
+void LTexture_atomic_operations::setBlendMode( SDL_BlendMode blending )
 {
 	//Set blending function
 	SDL_SetTextureBlendMode( mTexture, blending );
 }
 		
-void LTexture_semaphores::setAlpha( Uint8 alpha )
+void LTexture_atomic_operations::setAlpha( Uint8 alpha )
 {
 	//Modulate texture alpha
 	SDL_SetTextureAlphaMod( mTexture, alpha );
 }
 
-void LTexture_semaphores::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
+void LTexture_atomic_operations::render( int x, int y, SDL_Rect* clip, double angle, SDL_Point* center, SDL_RendererFlip flip )
 {
 	//Set rendering space and render to screen
 	SDL_Rect renderQuad = { x, y, mWidth, mHeight };
@@ -296,26 +296,26 @@ void LTexture_semaphores::render( int x, int y, SDL_Rect* clip, double angle, SD
 	}
 
 	//Render to screen
-	SDL_RenderCopyEx( gRenderer_semaphores, mTexture, clip, &renderQuad, angle, center, flip );
+	SDL_RenderCopyEx( gRenderer_atomic_operations, mTexture, clip, &renderQuad, angle, center, flip );
 }
 
-void LTexture_semaphores::setAsRenderTarget()
+void LTexture_atomic_operations::setAsRenderTarget()
 {
 	//Make self render target
-	SDL_SetRenderTarget( gRenderer_semaphores, mTexture );
+	SDL_SetRenderTarget( gRenderer_atomic_operations, mTexture );
 }
 
-int LTexture_semaphores::getWidth()
+int LTexture_atomic_operations::getWidth()
 {
 	return mWidth;
 }
 
-int LTexture_semaphores::getHeight()
+int LTexture_atomic_operations::getHeight()
 {
 	return mHeight;
 }
 
-bool LTexture_semaphores::lockTexture()
+bool LTexture_atomic_operations::lockTexture()
 {
 	bool success = true;
 
@@ -338,7 +338,7 @@ bool LTexture_semaphores::lockTexture()
 	return success;
 }
 
-bool LTexture_semaphores::unlockTexture()
+bool LTexture_atomic_operations::unlockTexture()
 {
 	bool success = true;
 
@@ -359,12 +359,12 @@ bool LTexture_semaphores::unlockTexture()
 	return success;
 }
 
-void* LTexture_semaphores::getPixels()
+void* LTexture_atomic_operations::getPixels()
 {
 	return mPixels;
 }
 
-void LTexture_semaphores::copyPixels( void* pixels )
+void LTexture_atomic_operations::copyPixels( void* pixels )
 {
 	//Texture is locked
 	if( mPixels != NULL )
@@ -374,12 +374,12 @@ void LTexture_semaphores::copyPixels( void* pixels )
 	}
 }
 
-int LTexture_semaphores::getPitch()
+int LTexture_atomic_operations::getPitch()
 {
 	return mPitch;
 }
 
-Uint32 LTexture_semaphores::getPixel32( unsigned int x, unsigned int y )
+Uint32 LTexture_atomic_operations::getPixel32( unsigned int x, unsigned int y )
 {
     //Convert the pixels to 32 bit
     Uint32 *pixels = (Uint32*)mPixels;
@@ -388,7 +388,7 @@ Uint32 LTexture_semaphores::getPixel32( unsigned int x, unsigned int y )
     return pixels[ ( y * ( mPitch / 4 ) ) + x ];
 }
 
-bool init_semaphores()
+bool init_atomic_operations()
 {
 	//Initialization flag
 	bool success = true;
@@ -408,8 +408,8 @@ bool init_semaphores()
 		}
 
 		//Create window
-		gWindow_semaphores = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( gWindow_semaphores == NULL )
+		gWindow_atomic_operations = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( gWindow_atomic_operations == NULL )
 		{
 			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
 			success = false;
@@ -417,8 +417,8 @@ bool init_semaphores()
 		else
 		{
 			//Create renderer for window
-			gRenderer_semaphores = SDL_CreateRenderer( gWindow_semaphores, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
-			if( gRenderer_semaphores == NULL )
+			gRenderer_atomic_operations = SDL_CreateRenderer( gWindow_atomic_operations, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC );
+			if( gRenderer_atomic_operations == NULL )
 			{
 				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
 				success = false;
@@ -426,7 +426,7 @@ bool init_semaphores()
 			else
 			{
 				//Initialize renderer color
-				SDL_SetRenderDrawColor( gRenderer_semaphores, 0xFF, 0xFF, 0xFF, 0xFF );
+				SDL_SetRenderDrawColor( gRenderer_atomic_operations, 0xFF, 0xFF, 0xFF, 0xFF );
 
 				//Initialize PNG loading
 				int imgFlags = IMG_INIT_PNG;
@@ -442,16 +442,13 @@ bool init_semaphores()
 	return success;
 }
 
-bool loadMedia_semaphores()
+bool loadMedia_atomic_operations()
 {
-	//Initialize semaphore
-	gDataLock_semaphores = SDL_CreateSemaphore( 1 );
-
 	//Loading success flag
 	bool success = true;
 	
 	//Load splash texture
-	if( !gSplashTexture_semaphores.loadFromFile( "splash.png" ) )
+	if( !gSplashTexture_atomic_operations.loadFromFile( "splash.png" ) )
 	{
 		printf( "Failed to load splash texture!\n" );
 		success = false;
@@ -460,27 +457,23 @@ bool loadMedia_semaphores()
 	return success;
 }
 
-void close_semaphores()
+void close_atomic_operations()
 {
 	//Free loaded images
-	gSplashTexture_semaphores.free();
-
-	//Free semaphore
-	SDL_DestroySemaphore( gDataLock_semaphores);
-	gDataLock_semaphores = NULL;
+	gSplashTexture_atomic_operations.free();
 
 	//Destroy window	
-	SDL_DestroyRenderer( gRenderer_semaphores);
-	SDL_DestroyWindow( gWindow_semaphores);
-	gWindow_semaphores = NULL;
-	gRenderer_semaphores = NULL;
+	SDL_DestroyRenderer( gRenderer_atomic_operations);
+	SDL_DestroyWindow( gWindow_atomic_operations);
+	gWindow_atomic_operations = NULL;
+	gRenderer_atomic_operations = NULL;
 
 	//Quit SDL subsystems
 	IMG_Quit();
 	SDL_Quit();
 }
 
-int worker_semaphores( void* data )
+int worker_atomic_operations( void* data )
 {
 	printf( "%s starting...\n", data );
 
@@ -494,19 +487,19 @@ int worker_semaphores( void* data )
 		SDL_Delay( 16 + rand() % 32 );
 		
 		//Lock
-		SDL_SemWait( gDataLock_semaphores);
+		SDL_AtomicLock( &gDataLock_atomic_operations);
 
 		//Print pre work data
-		printf( "%s gets %d\n", data, gData_semaphores);
+		printf( "%s gets %d\n", data, gData_atomic_operations);
 
 		//"Work"
-		gData_semaphores = rand() % 256;
+		gData_atomic_operations = rand() % 256;
 
 		//Print post work data
-		printf( "%s sets %d\n\n", data, gData_semaphores);
+		printf( "%s sets %d\n\n", data, gData_atomic_operations);
 		
 		//Unlock
-		SDL_SemPost( gDataLock_semaphores);
+		SDL_AtomicUnlock( &gDataLock_atomic_operations);
 
 		//Wait randomly
 		SDL_Delay( 16 + rand() % 640 );
@@ -518,17 +511,17 @@ int worker_semaphores( void* data )
 }
 
 
-int main_semaphores( int argc, char* args[] )
+int main( int argc, char* args[] )
 {
 	//Start up SDL and create window
-	if( !init_semaphores() )
+	if( !init_atomic_operations() )
 	{
 		printf( "Failed to initialize!\n" );
 	}
 	else
 	{
 		//Load media
-		if( !loadMedia_semaphores() )
+		if( !loadMedia_atomic_operations() )
 		{
 			printf( "Failed to load media!\n" );
 		}
@@ -542,12 +535,12 @@ int main_semaphores( int argc, char* args[] )
 
 			//Run the threads
 			srand( SDL_GetTicks() );
-			SDL_Thread* threadA = SDL_CreateThread( worker_semaphores, "Thread A", (void*)"Thread A" );
+			SDL_Thread* threadA = SDL_CreateThread( worker_atomic_operations, "Thread A", (void*)"Thread A" );
 			SDL_Delay( 16 + rand() % 32 );
-			SDL_Thread* threadB = SDL_CreateThread( worker_semaphores, "Thread B", (void*)"Thread B" );
+			SDL_Thread* threadB = SDL_CreateThread( worker_atomic_operations, "Thread B", (void*)"Thread B" );
 			
 			//While application is running
-			while( !quit )
+			while( quit == false )
 			{
 				//Handle events on queue
 				while( SDL_PollEvent( &e ) != 0 )
@@ -560,14 +553,14 @@ int main_semaphores( int argc, char* args[] )
 				}
 
 				//Clear screen
-				SDL_SetRenderDrawColor( gRenderer_semaphores, 0xFF, 0xFF, 0xFF, 0xFF );
-				SDL_RenderClear( gRenderer_semaphores);
+				SDL_SetRenderDrawColor( gRenderer_atomic_operations, 0xFF, 0xFF, 0xFF, 0xFF );
+				SDL_RenderClear( gRenderer_atomic_operations);
 
 				//Render splash
-				gSplashTexture_semaphores.render( 0, 0 );
+				gSplashTexture_atomic_operations.render( 0, 0 );
 
 				//Update screen
-				SDL_RenderPresent( gRenderer_semaphores);
+				SDL_RenderPresent( gRenderer_atomic_operations);
 			}
 
 			//Wait for threads to finish
@@ -577,7 +570,7 @@ int main_semaphores( int argc, char* args[] )
 	}
 
 	//Free resources and close SDL
-	close_semaphores();
+	close_atomic_operations();
 
 	return 0;
 }
